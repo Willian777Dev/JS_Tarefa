10ce32ce9c8f3038f33da3c57134cad4b87eeef5
Wilian Koppe <williankoppe70@gmail.com>
2025-01-31 21:29:58 -0300

    Resolução de Exercício

---
+++ array.js
@@ -0,0 +1,8 @@
+let numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+console.log(numeros.length)
+
+let primeiroNumero = numeros[0]
+console.log(primeiroNumero)
+
+let ultimoNumero = numeros[15]
+console.log(ultimoNumero)
---
+++ mdc.js
@@ -0,0 +1,3 @@
+const divisão = (a = 15, b = 20, c = 5) => a / b / c
+console.log(divisão(15/5))
+console.log(divisão(20/5))
